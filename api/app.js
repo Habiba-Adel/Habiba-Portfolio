f2a8edd273772c36d530872cee47f21dc3afaf16
habiba abdelgowad <habibaabdelgowad9@gmail.com>
2026-01-31 03:47:36 +0200

    yarab

--- api/app.js
+++ api/app.js
@@ -10,8 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, "..")));
-
+app.use(express.static(path.join(__dirname, "..", "public")));
 // ===== Rate limit =====
 const contactLimiter = rateLimit({
   windowMs: 60 * 1000,
